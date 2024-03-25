321f9acb627597e6960511fb6ad774a9d5112206
humairamutiaa <144106937+humairamutiaa@users.noreply.github.com>
2024-03-25 22:18:05 +0700

    Add files via upload

---
+++ Job Sheet 1/Introduction.cpp
@@ -0,0 +1,8 @@
+//Created by Humaira Mutia
+
+#include <stdio.h>
+int main()
+{
+	printf("\nWelcome to the world of C"); //prints the message on the screen
+	return 0; //returns a value 0 to the operating system
+}
